6f2fa33f43d09f38590c3c8db519009f7809a8a2
Pavdem2012 <pavdem2012@gmail.com>
2023-05-11 18:22:03 +0700

    Added allure reporting options

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,19 @@ module.exports =  {
     testEnvironment: 'node',
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.js', 'specs/**/*.js'],
-    reporters: ['default',
-        ["./node_modules/jest-html-reporter", {
-            "pageTitle": "Отчет о прохождении тестов",
-            "includeFailureMsg": true,
+    // reporters: ['default',
+    //     ["./node_modules/jest-html-reporter", {
+    //         "pageTitle": "Отчет о прохождении тестов",
+    //         "includeFailureMsg": true,
 
-        }]
-    ],
+    //     }]
+    // ],
+    reporters: ['default', 'jest-allure', "./node_modules/jest-html-reporter", {
+                "pageTitle": "Отчет о прохождении тестов",
+                "includeFailureMsg": true,
+            }],
+    testRunner: 'jest-jasmine2',
+    setupFilesAfterEnv: ['jest-extended','jest-allure/dist/setup'],
     moduleFileExtensions: ['js', 'json'],
     transform: {
       '^.+\\.jsx?$': 'babel-jest',
--- specs/api.test.js
+++ specs/api.test.js
@@ -1,4 +1,6 @@
 import config from "../framework/config/config.js";
+import {Severity, Status} from 'jest-allure/dist/Reporter';
+
 import {
     generateBadPassRequestData,
     generateCorrectRequestData,
@@ -16,7 +18,10 @@ import {
     updateBook,
     deleteBook
 } from "../framework/services/bookService";
+//import 'jest-extended';
 import expect from "expect";
+
+//import { reporters } from "../jest.config.js";
 
 let ISBN1;
 let ISBN0;
@@ -24,6 +29,7 @@ let UUID;
 let token;
 let responce;
 let validRequestData = generateCorrectRequestData();
+
 
 //Тест для проверки все ли работает
 // test('should return correct data from API', async () => {
@@ -36,7 +42,9 @@ let validRequestData = generateCorrectRequestData();
 //     expect(response.data.completed).toEqual(false);
 // });
 
-
+beforeEach(() =>{
+    reporter.addEnvironment('URL: ',`${config.BOOKSTORE_BASE_URL}`)
+})
 /**
  * Тесты на создание пользователя и получения информации о нем
  */
@@ -46,36 +54,88 @@ describe('API tests create user', () => {
      * Проверка "Создание пользователя c ошибкой, пароль не подходит"
      */
 
-    test('error message when sending empty password', async () => {
+    test('error message when sending  no valid password', async () => {
+        reporter.epic('Создание пользователя POST/Account/v1/User')
+                .feature('возврат 400 статус-кода при отправке не правильного пароля')
+                .description('возврат 400 статус-кода при отправке не правильного пароля')
+                .startStep(`register as ${JSON.stringify(generateBadPassRequestData())}`)
         responce = await wtBearerResp({requestData: generateBadPassRequestData(), path:config.USER_ACC_PATH});
-        expect(responce.status).toEqual(400);
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the responce')
+                .startStep('Checking the returned status code')
+        expect(responce.status).toBe(400);
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned status text')
         expect(responce.statusText).toBe('Bad Request')
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned responce body code')
         expect(responce.data.code).toBe('1300');
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned responce body message')
         expect(responce.data.message).toBe('Passwords must have at least one non alphanumeric character, one digit (\'0\'-\'9\'), one uppercase (\'A\'-\'Z\'), one lowercase (\'a\'-\'z\'), one special character and Password must be eight characters or longer.');
+        reporter.endStep(Status.Passed)
+                .endStep(Status.Passed)
+
+
     });
     /**
      * Проверка "Создание пользователя успешно"
      */
     test('should create a new user', async () => {
+        reporter.epic('Создание пользователя POST/Account/v1/User')
+                .feature('возврат 201 статус-кода и валидного тела ответа при отправке валидного запроса')
+                .description('возврат 201 статус-кода и валидного тела ответа при отправке валидного запроса')
+                .severity(Severity.Normal)
+        reporter.startStep(`register as ${JSON.stringify(generateCorrectRequestData())}`)
         responce = await wtBearerResp({requestData:validRequestData,  path:config.USER_ACC_PATH});
         UUID = responce.data.userID;
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the responce')
+                .startStep('Checking the returned status code')
         expect(responce.status).toEqual(201);
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned status text')
         expect(responce.statusText).toBe('Created')
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned userID toBeDefined')
         expect(responce.data.userID).toBeDefined();
+        reporter.endStep(Status.Passed)
+                .startStep(`Checking the returned username toEqual ${validRequestData.userName}`)
         expect(responce.data.username).toEqual(validRequestData.userName);
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned books toBeDefined')
         expect(responce.data.books).toBeDefined();
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned books toBeArray')
         expect(Array.isArray(responce.data.books)).toBe(true);
+        reporter.endStep(Status.Passed)
+                .endStep(Status.Passed)
     });
     /**
      * Проверка "Создание пользователя c ошибкой, логин уже используется"
      */
 
     test('should return error message when sending existing userName', async () => {
+        reporter.epic('Создание пользователя POST/Account/v1/User')
+                .feature('возврат 406 статус-кода и валидного тела ответа при отправке повторного валидного запроса')
+                .description('возврат 406 статус-кода и валидного тела ответа при отправке повторного валидного запроса')
+                .startStep(`register as ${JSON.stringify(generateCorrectRequestData())}`)
         responce = await wtBearerResp({requestData:validRequestData,  path:config.USER_ACC_PATH});
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the responce')
+                .startStep('Checking the returned status code')
         expect(responce.status).toBe(406);
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned status text')
         expect(responce.statusText).toBe('Not Acceptable')
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned responce body code')
         expect(responce.data.code).toBe('1204');
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned responce body message')
         expect(responce.data.message).toBe('User exists!');
+        reporter.endStep(Status.Passed)
+                .endStep(Status.Passed)
     });
 });
 
@@ -89,22 +149,51 @@ describe('API tests generate token', () => {
      * Проверка "Генерация токена c ошибкой"
      */
     test('returns an error message when body are not provided', async () => {
+        reporter.epic('Генерация токена POST/Account/v1/GenerateToken')
+                .feature('возврат 400 статус-кода и валидного тела ответа при отправке пустых полей в теле запроса')
+                .description('возврат 400 статус-кода и валидного тела ответа при отправке  пустых полей в теле запроса')
+                .startStep('send a request')
         responce = await wtBearerResp({requestData: generateNullBodyRequestData(), path:config.GEN_ACC_TOKEN_PATH});
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the responce')
+                .startStep('Checking the returned status code')
         expect(responce.status).toBe(400);
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned status text')
         expect(responce.statusText).toBe('Bad Request')
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned responce body code')
         expect(responce.data.code).toBe('1200');
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned responce body message')
         expect(responce.data.message).toBe('UserName and Password required.');
-
+        reporter.endStep(Status.Passed)
+                .endStep(Status.Passed)
     });
     /**
      * Проверка "Генерация токена успешно"
      */
     test('Should generate token for valid user', async () => {
+        reporter.epic('Генерация токена POST/Account/v1/GenerateToken')
+                .feature('возврат 200 статус-кода и валидного тела ответа при отправке валидного запроса')
+                .description('возврат 200 статус-кода и валидного тела ответа при отправке валидного запроса')
+                .startStep('send a request')
         responce = await wtBearerResp({requestData:validRequestData, path:config.GEN_ACC_TOKEN_PATH});
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the responce')
+                .startStep('Checking the returned status code')
         expect(responce.status).toBe(200);
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned status text')
         expect(responce.statusText).toBe('OK')
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned data status')
         expect(responce.data.status).toBe('Success');
+        reporter.endStep(Status.Passed)
+                .startStep('Checking the returned data token toBeTruthy')
         expect(responce.data.token).toBeTruthy();
+        reporter.endStep(Status.Passed)
+                .endStep(Status.Passed)
         token = responce.data.token;
     });
 
