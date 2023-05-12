238377cddab7ca15a81319ea8ce4b4e46ddc3a15
Павел Деменчук <demenchuk.po@dclouds.ru>
2023-05-12 11:28:35 +0700

    fixed jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,12 +9,18 @@ module.exports =  {
 
     //     }]
     // ],
-    reporters: ['default', 'jest-allure', "./node_modules/jest-html-reporter", {
-                "pageTitle": "Отчет о прохождении тестов",
-                "includeFailureMsg": true,
+    reporters: ['default',
+        ["./node_modules/jest-html-reporter", {
+            "pageTitle": "Отчет о прохождении тестов",
+            "includeFailureMsg": true,
             }],
+            'jest-allure'
+        ],
     testRunner: 'jest-jasmine2',
-    setupFilesAfterEnv: ['jest-extended','jest-allure/dist/setup'],
+    setupFilesAfterEnv: [
+        'jest-extended',
+        'jest-allure/dist/setup'
+        ],
     moduleFileExtensions: ['js', 'json'],
     transform: {
       '^.+\\.jsx?$': 'babel-jest',
