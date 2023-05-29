e20643079bef32e1c91568884d08b88906bd9b47
Павел Деменчук <demenchuk.po@dclouds.ru>
2023-05-29 16:46:06 +0700

    fix workflow

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports =  {
     // ],
     reporters: ['default',
         ["jest-html-reporters", {
-            "publicPath": "./jest-html-report",
+            "publicPath": "report/jest-html-report",
             "filename": "index.html"
         }
         ],
