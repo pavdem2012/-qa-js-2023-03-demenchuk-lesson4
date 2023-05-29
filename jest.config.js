2f0fac7056e59ae2a137c4c92523eab7b557e237
Павел Деменчук <demenchuk.po@dclouds.ru>
2023-05-29 10:59:21 +0700

    fix workflow

--- jest.config.js
+++ jest.config.js
@@ -12,13 +12,9 @@ module.exports =  {
     reporters: ['default',
         ["jest-html-reporters", {
             "publicPath": "./jest-html-report",
-            "filename": "index.html"
+            "filename": "report.html"
         }
         ],
-        ["./node_modules/jest-html-reporter", {
-            "pageTitle": "Отчет о прохождении тестов",
-            "includeFailureMsg": true,
-        }],
             'jest-allure'
         ],
     testRunner: 'jest-jasmine2',
